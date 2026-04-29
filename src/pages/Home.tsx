5695482245e6100064f3e868a0d24999847f6710
Pierre14290 <126866896+Pierre14290@users.noreply.github.com>
2026-04-29 20:14:25 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -94,7 +94,7 @@ export default function Home({ onNavigate }: HomeProps) {
           </div>
 
           <h1 className="hero-title" style={{ marginBottom: '1rem' }}>
-            <span style={{ color: '#F0FDF4' }}>LA</span><span style={{ color: '#F59E0B' }}> RUCHE campus</span>
+            <span style={{ color: '#F0FDF4' }}>LA</span><span style={{ color: '#F59E0B' }}> RUCHE</span>
             <br />
             <span style={{ color: '#F59E0B' }}>campus</span>
           </h1>
